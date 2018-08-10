3be7d4480c118c248cddc61a8e78c1f917da8caf
Aubrey Chipman <achipman@netflix.com>
2018-08-10 15:56:34 -0700

    Dependency insight - update assertions for second order contributors

--- dependency-insight/src/test/groovy/SecondOrderContributorsSpec.groovy
+++ dependency-insight/src/test/groovy/SecondOrderContributorsSpec.groovy
@@ -1,3 +1,4 @@
+import com.google.common.collect.ImmutableMap
 import nebula.test.dependencies.maven.Pom
 
 /**
@@ -60,40 +61,31 @@ dependencies {
                 "Mercury\nTasks: ${String.join(' ', mercuryDependencyInsightTasks)}\n\n${mercuryResult.output}\n\n",
                 "Venus\nTasks: ${String.join(' ', venusDependencyInsightTasks)}\n\n${venusResult.output}\n\n")
 
+        def expectedSecondOrderConflictStatement = '\\--- planet:venus:2.0.1 (conflict resolution between versions'
+        docWriter.addAssertionToDoc("[mercury dependencyInsight] contains '$expectedSecondOrderConflictStatement'")
+        assert mercuryResult.output.contains(expectedSecondOrderConflictStatement)
+
+        docWriter.writeFooter('completed assertions')
     }
 
     private void setupDependenciesInLocalRepo() {
         repo.mkdirs()
+        setupLocalDependency(mercury, '1.0.0', Collections.emptyMap())
+        setupLocalDependency(mercury, '1.0.1', Collections.emptyMap())
+        setupLocalDependency(mercury, '1.0.2', Collections.emptyMap())
+        setupLocalDependency(venus, '2.0.0', ImmutableMap.of(mercury, '1.0.0'))
+        setupLocalDependency(venus, '2.0.1', ImmutableMap.of(mercury, '1.0.1'))
+        setupLocalDependency(earth, '3.0.0', ImmutableMap.of(venus, '2.0.0'))
+        setupLocalDependency(mars, '4.0.0', ImmutableMap.of(venus, '2.0.1'))
+        setupLocalDependency(jupiter, '5.0.0', ImmutableMap.of(mercury, '1.0.2'))
+    }
 
-        def mercury100 = new Pom('planet', mercury, '1.0.0')
-        ArtifactHelpers.setupSamplePomWith(repo, mercury100, mercury100.generate())
-
-        def mercury101 = new Pom('planet', mercury, '1.0.1')
-        ArtifactHelpers.setupSamplePomWith(repo, mercury101, mercury101.generate())
-
-        def mercury102 = new Pom('planet', mercury, '1.0.2')
-        ArtifactHelpers.setupSamplePomWith(repo, mercury102, mercury102.generate())
-
-
-        def venus200 = new Pom('planet', venus, '2.0.0')
-        venus200.addDependency('planet', mercury, '1.0.0')
-        ArtifactHelpers.setupSamplePomWith(repo, venus200, venus200.generate())
-
-        def venus201 = new Pom('planet', venus, '2.0.1')
-        venus201.addDependency('planet', mercury, '1.0.1')
-        ArtifactHelpers.setupSamplePomWith(repo, venus201, venus201.generate())
-
-
-        def earth300 = new Pom('planet', earth, '3.0.0')
-        earth300.addDependency('planet', venus, '2.0.0')
-        ArtifactHelpers.setupSamplePomWith(repo, earth300, earth300.generate())
-
-        def mars400 = new Pom('planet', mars, '4.0.0')
-        mars400.addDependency('planet', venus, '2.0.1')
-        ArtifactHelpers.setupSamplePomWith(repo, mars400, mars400.generate())
-
-        def jupiter500 = new Pom('planet', jupiter, '5.0.0')
-        jupiter500.addDependency('planet', mercury, '1.0.2')
-        ArtifactHelpers.setupSamplePomWith(repo, jupiter500, jupiter500.generate())
+    private void setupLocalDependency(String artifactName, String version, Map<String, String> dependencies) {
+        def group = 'planet'
+        def pom = new Pom(group, artifactName, version)
+        for (Map.Entry<String, String> entry : dependencies) {
+            pom.addDependency(group, entry.key, entry.value)
+        }
+        ArtifactHelpers.setupSamplePomWith(repo, pom, pom.generate())
     }
 }
