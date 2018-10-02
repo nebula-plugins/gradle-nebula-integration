20052668f7d86fc0dd1461406772fef95d37c346
Aubrey Chipman <achipman@netflix.com>
2018-10-02 13:48:00 -0700

    Prefer & require: test with recommendations

--- require-prefer/src/test/groovy/RequirePreferSpec.groovy
+++ require-prefer/src/test/groovy/RequirePreferSpec.groovy
@@ -1,3 +1,4 @@
+import nebula.test.dependencies.maven.ArtifactType
 import nebula.test.dependencies.maven.Pom
 import org.assertj.core.util.Lists
 import org.gradle.util.VersionNumber
@@ -26,6 +27,7 @@ class RequirePreferSpec extends TestKitSpecification {
     String second = 'coast-redwood'
 
     String group = 'tree'
+    def versionRangeIndicators = ['[', ']', '(', ')']
     File repo
 
     def setup() {
@@ -35,10 +37,10 @@ class RequirePreferSpec extends TestKitSpecification {
     }
 
     @Unroll
-    def "#title"() {
+    def "1. #title"() {
         given:
-        addSubproject(first, buildFileWithDependencyVersions(firstRequireVersion, preferFirstVersion))
-        addSubproject(second, buildFileWithDependencyVersions(secondRequireVersion, preferSecondVersion))
+        addSubproject(first, buildFileWithDependencyVersions(require1, prefer1))
+        addSubproject(second, buildFileWithDependencyVersions(require2, prefer2))
 
         buildFile << simpleParentMultiProjectBuildFile()
 
@@ -53,16 +55,16 @@ class RequirePreferSpec extends TestKitSpecification {
         docWriter.writeCleanedUpBuildOutput(
                 "Tasks: ${String.join(' ', tasks)}\n\n" +
                         "Scenario: $title\n" +
-                        "Preferred version(s): $preferFirstVersion, $preferSecondVersion\n\n" +
+                        "Preferred version(s): $prefer1, $prefer2\n\n" +
                         "${result.output}\n\n")
 
-        def versionRangeIndicators = ['[', ']', '(', ')']
-        def firstHasDynamicDep = versionRangeIndicators.any { firstRequireVersion.contains(it) }
-        def secondHasDynamicDep = versionRangeIndicators.any { secondRequireVersion.contains(it) }
+        def firstHasDynamicDep = versionRangeIndicators.any { require1.contains(it) }
+        def secondHasDynamicDep = versionRangeIndicators.any { require2.contains(it) }
 
-        if (!firstHasDynamicDep && !secondHasDynamicDep) {
-            def winningVersion = returnHigherOf(firstRequireVersion, secondRequireVersion)
-            def losingVersion = Lists.newArrayList(firstRequireVersion, secondRequireVersion)
+        if (!firstHasDynamicDep || !secondHasDynamicDep) {
+            String winningVersion = finalVersion
+
+            def losingVersion = Lists.newArrayList(require1, require2)
             losingVersion.remove(winningVersion)
 
             def winningReasonResultingVersion = "$group:$dep:$winningVersion"
@@ -75,11 +77,11 @@ class RequirePreferSpec extends TestKitSpecification {
 
 
         } else {
-            def firstReasonResultingVersion = "$group:$dep:$firstRequireVersion -> $resultingVersion"
+            def firstReasonResultingVersion = "$group:$dep:$require1 -> $finalVersion"
             docWriter.addAssertionToDoc("First dep resulting version: '$firstReasonResultingVersion'")
             assert result.output.contains(firstReasonResultingVersion)
 
-            def secondReasonResultingVersion = "$group:$dep:$secondRequireVersion -> $resultingVersion"
+            def secondReasonResultingVersion = "$group:$dep:$require2 -> $finalVersion"
             docWriter.addAssertionToDoc("Second dep resulting version: '$secondReasonResultingVersion'")
             assert result.output.contains(secondReasonResultingVersion)
         }
@@ -87,15 +89,118 @@ class RequirePreferSpec extends TestKitSpecification {
         docWriter.writeFooter('completed assertions')
 
         where:
-        preferFirstVersion | preferSecondVersion | firstRequireVersion | secondRequireVersion | resultingVersion | title
-        '1.5'              | null                | '[1.2, 2.0)'        | '[1.2, 2.0)'         | '1.5'            | 'one preference in required range'
-        '1.5'              | '1.6'               | '[1.2, 2.0)'        | '[1.2, 2.0)'         | '1.6'            | 'two preferences in required range - higher is chosen'
-        '1.5'              | null                | '[1.2, 2.0)'        | '[2.0, 3.0)'         | '2.9'            | 'conflict resolution - higher range does not have a preference'
-        '1.5'              | null                | '[1.4, 2.0)'        | '[1.0, 1.2)'         | '1.5'            | 'conflict resolution - higher range has a preference'
-        '1.5'              | null                | '1.0'               | '2.0'                | '2.0'            | 'preference not used with static dependency'
+        prefer1 | prefer2 | require1     | require2     | finalVersion | title
+        '1.5'   | null    | '[1.2, 2.0)' | '[1.2, 2.0)' | '1.5'        | 'one preference in required range'
+        '1.5'   | '1.6'   | '[1.2, 2.0)' | '[1.2, 2.0)' | '1.6'        | 'two preferences in required range - higher is chosen'
+        '1.5'   | null    | '[1.2, 2.0)' | '[2.0, 3.0)' | '2.9'        | 'conflict resolution - higher range does not have a preference'
+        '1.5'   | null    | '[1.4, 2.0)' | '[1.0, 1.2)' | '1.5'        | 'conflict resolution - higher range has a preference'
+        '1.5'   | null    | '[1.2, 2.0)' | '2.9'        | '2.9'        | 'conflict resolution with range and static - higher does not use preference'
+        '1.5'   | null    | '[1.4, 2.0)' | '1.2'        | '1.5'        | 'conflict resolution with range and static - preference is higher'
+        '1.5'   | null    | '1.0'        | '2.0'        | '2.0'        | 'preference not used with static dependency'
     }
 
-    private def simpleParentMultiProjectBuildFile() {
+    @Unroll
+    def '2. #title'() {
+        def shouldUseFirstProject = prefer1 != null && require1 != null && prefer1 != '' & require1 != ''
+        given:
+        if (shouldUseFirstProject) {
+            addSubproject(first, buildFileWithDependencyVersions(require1, prefer1))
+        }
+        addSubproject(second, """
+plugins {
+    id 'java-library'
+}
+
+group '$group'
+version '1.0'
+
+dependencies {
+    api platform('sample:bom:1.0.0')
+    api '$group:$dep'
+    api ('$group:$dep') {
+        version {
+            prefer '$bomVersion'
+        }
+    }
+}
+
+repositories {
+    jcenter()
+    maven { url '${repo.absolutePath}' }
+}
+""")
+        buildFile << simpleParentMultiProjectBuildFile(shouldUseFirstProject)
+
+        when:
+        def tasks = ['dependencyInsight', '--dependency', "$dep"]
+        def result = runTasks(*tasks)
+
+        DocWriter docWriter = new DocWriter(title.replace(' ', '-'), projectDir, 'misc')
+
+        then:
+        docWriter.writeProjectFiles()
+        docWriter.writeCleanedUpBuildOutput(
+                "Tasks: ${String.join(' ', tasks)}\n\n" +
+                        "Scenario: $title\n" +
+                        "Preferred version(s): $prefer1, $bomVersion\n\n" +
+                        "${result.output}\n\n")
+
+        def firstHasDynamicDep = versionRangeIndicators.any { require1.contains(it) }
+        def secondHasDynamicDep = versionRangeIndicators.any { require2.contains(it) }
+
+        if (!firstHasDynamicDep || !secondHasDynamicDep) {
+            String winningVersion = finalVersion
+
+            def losingVersion = Lists.newArrayList(require1, require2)
+            losingVersion.remove(winningVersion)
+
+            def winningReasonResultingVersion = "$group:$dep:$winningVersion"
+            docWriter.addAssertionToDoc("Winning dep resulting version: '$winningReasonResultingVersion'")
+            assert result.output.contains(winningReasonResultingVersion)
+
+
+            def losingReasonResultingVersion
+            if (losingVersion.any { (it == '') }) {
+                losingReasonResultingVersion = "$group:$dep -> $winningVersion"
+            } else {
+                losingReasonResultingVersion = "$group:$dep:${losingVersion.first()} -> $winningVersion"
+            }
+            docWriter.addAssertionToDoc("Losing dep resulting version: '$losingReasonResultingVersion'")
+            assert result.output.contains(losingReasonResultingVersion)
+
+
+        } else {
+            def firstReasonResultingVersion = "$group:$dep:$require1 -> $finalVersion"
+            docWriter.addAssertionToDoc("First dep resulting version: '$firstReasonResultingVersion'")
+            assert result.output.contains(firstReasonResultingVersion)
+
+            def secondReasonResultingVersion = "$group:$dep:$require2 -> $finalVersion"
+            docWriter.addAssertionToDoc("Second dep resulting version: '$secondReasonResultingVersion'")
+            assert result.output.contains(secondReasonResultingVersion)
+        }
+
+        docWriter.writeFooter('completed assertions')
+
+        where:
+        prefer1 | bomVersion | require1     | require2 | finalVersion | title
+        ''      | '1.7'      | ''           | '1.6'    | '1.7'        | 'preference with bom - prefer > bom'
+        '1.5'   | '1.7'      | '[1.2, 2.0)' | '1.6'    | '1.7'        | 'preference with bom - prefer > bom & > other prefer'
+    }
+
+    def createBom(String depVersion) {
+        repo.mkdirs()
+
+        def localBom = new Pom('sample', 'bom', '1.0.0', ArtifactType.POM)
+        localBom.addManagementDependency(group, dep, depVersion)
+        ArtifactHelpers.setupSamplePomWith(repo, localBom, localBom.generate())
+    }
+
+    private def simpleParentMultiProjectBuildFile(boolean shouldUseFirstProject) {
+        def firstProject = ''
+        if (shouldUseFirstProject) {
+            firstProject = "compile project(':$first')"
+        }
+
         """
 apply plugin: 'java'
 
@@ -104,7 +209,7 @@ repositories {
 }
 
 dependencies {
-    compile project(':$first')
+    $firstProject
     compile project(':$second')
 }"""
     }
