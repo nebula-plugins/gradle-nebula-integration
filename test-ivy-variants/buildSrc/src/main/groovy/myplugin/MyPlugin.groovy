bedb5b753030ade9ac3219bdfe7eea00effaa9bf
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2020-03-06 15:46:48 -0800

    Show example of excludes not being honored by variant

--- test-ivy-variants/buildSrc/src/main/groovy/myplugin/MyPlugin.groovy
+++ test-ivy-variants/buildSrc/src/main/groovy/myplugin/MyPlugin.groovy
@@ -8,10 +8,10 @@ import org.gradle.api.artifacts.CacheableRule
 import org.gradle.api.artifacts.ComponentMetadataContext
 import org.gradle.api.artifacts.ComponentMetadataRule
 import org.gradle.api.artifacts.VariantMetadata
+import org.gradle.api.artifacts.ivy.IvyModuleDescriptor
 import org.gradle.api.attributes.Category
 import org.gradle.api.attributes.LibraryElements
 import org.gradle.api.attributes.Usage
-import org.gradle.api.initialization.dsl.ScriptHandler
 import org.gradle.api.model.ObjectFactory
 
 import javax.inject.Inject
@@ -19,12 +19,6 @@ import javax.inject.Inject
 class MyPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        project.buildscript { ScriptHandler b ->
-            b.dependencies.components.each {
-                it
-            }
-            b.dependencies.components.all(IvyVariantDerivationRule)
-        }
         project.dependencies.components.all(IvyVariantDerivationRule)
     }
 
@@ -35,6 +29,10 @@ class MyPlugin implements Plugin<Project> {
         ObjectFactory getObjects() { }
 
         void execute(ComponentMetadataContext context) {
+            if(context.getDescriptor(IvyModuleDescriptor) == null) {
+                return
+            }
+
             context.details.maybeAddVariant('runtimeElements', 'runtime', new Action<VariantMetadata>() {
                 @Override
                 void execute(VariantMetadata variantMetadata) {
@@ -45,13 +43,17 @@ class MyPlugin implements Plugin<Project> {
                     }
                 }
             })
-            context.details.maybeAddVariant("apiElements", "compile") {
-                attributes {
-                    attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, getObjects().named(LibraryElements, LibraryElements.JAR))
-                    attribute(Category.CATEGORY_ATTRIBUTE, getObjects().named(Category, Category.LIBRARY))
-                    attribute(Usage.USAGE_ATTRIBUTE, getObjects().named(Usage, Usage.JAVA_API))
+
+             context.details.maybeAddVariant('apiElements', 'compile', new Action<VariantMetadata>() {
+                @Override
+                void execute(VariantMetadata variantMetadata) {
+                    variantMetadata.attributes {
+                        attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, getObjects().named(LibraryElements, LibraryElements.JAR))
+                        attribute(Category.CATEGORY_ATTRIBUTE, getObjects().named(Category, Category.LIBRARY))
+                        attribute(Usage.USAGE_ATTRIBUTE, getObjects().named(Usage, Usage.JAVA_API))
+                    }
                 }
-            }
+            })
         }
     }
 }
