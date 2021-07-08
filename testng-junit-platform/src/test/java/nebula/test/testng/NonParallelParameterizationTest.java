723ec8ad043884eae2e714d3dce6f9ff50b37b67
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2021-07-08 16:20:57 -0700

    enable failing test for testng

--- testng-junit-platform/src/test/java/nebula/test/testng/NonParallelParameterizationTest.java
+++ testng-junit-platform/src/test/java/nebula/test/testng/NonParallelParameterizationTest.java
@@ -3,7 +3,7 @@ package nebula.test.testng;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-@Test
+@Test(groups = "included")
 public class NonParallelParameterizationTest {
     @BeforeSuite
     public static void test() throws Exception {
@@ -20,13 +20,23 @@ public class NonParallelParameterizationTest {
         System.out.println("Before Method");
     }
 
-    @DataProvider(name = "numbers", indices = {0,1})
+    @DataProvider(name = "numbers")
     public static Integer[] numbers() {
         return new Integer[]{1, 3, 5, 7, 9, 11, 13, -5, -3, 15, Integer.MAX_VALUE};
     }
 
+    @DataProvider(name = "numbersSubset", indices = {2,5})
+    public static Integer[] subset() {
+        return numbers();
+    }
+
     @Test(description = "Test for odd numbers", dataProvider = "numbers")
     void isOdd_ShouldReturnTrueForOddNumbers(Integer number) {
+        Assert.assertTrue(Numbers.isOdd(number));
+    }
+
+    @Test(description = "Test for odd numbers subset", dataProvider = "numbersSubset")
+    void isOdd_ShouldReturnTrueForOddNumbersSubset(Integer number) {
         Assert.assertTrue(Numbers.isOdd(number));
     }
 }
