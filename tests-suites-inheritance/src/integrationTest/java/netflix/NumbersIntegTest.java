03a12c1a623a688ec9ed9016fc96439a1d04fe6b
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2022-03-02 11:57:09 -0600

    add more example for tests suites

--- tests-suites-inheritance/src/integrationTest/java/netflix/NumbersIntegTest.java
+++ tests-suites-inheritance/src/integrationTest/java/netflix/NumbersIntegTest.java
@@ -1,6 +1,5 @@
 package netflix;
 
-import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
 
 
@@ -12,11 +11,7 @@ import static org.junit.Assert.assertTrue;
 public class NumbersIntegTest {
     @Test
     public void isOdd_ShouldReturnTrueForOddNumbers() {
-        Map items = ImmutableMap.of("coin", 3, "glass", 4, "pencil", 1);
-
-        items.entrySet()
-                .stream()
-                .forEach(System.out::println);
+        //Map items = ImmutableMap.of("coin", 3, "glass", 4, "pencil", 1);
         assertTrue(Numbers.isOdd(3));
     }
 }
--- tests-suites-inheritance/src/test/java/netflix/NumbersTest.java
+++ tests-suites-inheritance/src/test/java/netflix/NumbersTest.java
@@ -1,12 +1,17 @@
 package netflix;
 
+import com.google.common.collect.ImmutableMap;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NumbersTest {
     @Test
     public void isOdd_ShouldReturnTrueForOddNumbers() {
+        Map items = ImmutableMap.of("coin", 3, "glass", 4, "pencil", 1);
+
         assertTrue(Numbers.isOdd(3));
     }
 }
